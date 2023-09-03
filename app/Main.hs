7cd642044da6a491ed4c9fdecbf600b909bf7c5d
Ethan Niser <thebestgamer2502@gmail.com>
2023-09-03 11:27:08 -0500

    done

--- app/Main.hs
+++ app/Main.hs
@@ -1,11 +1,12 @@
 import qualified Data.Map as M
+import qualified Data.Set as S
 
 main :: IO ()
 main = do
   file <- getLine
   contents <- readFile $ "app/" ++ "maze" ++ file ++ ".txt"
   case solve $ parse contents of
-    Just solution -> print solution
+    Just solution -> putStrLn $ "Solution found:\n" ++ show solution
     Nothing -> putStrLn "No solution found"
 
 type Graph = M.Map Int [Int]
@@ -19,7 +20,7 @@ data Maze = Maze
 newtype Solution = Solution {path :: [Int]}
 
 instance Show Solution where
-  show (Solution path) = show (length path) ++ "\n" ++ unwords (map show path)
+  show (Solution path) = "length- " ++ show (length path) ++ "\n" ++ unwords (map show path)
 
 parse :: String -> Maze
 parse input = case lines input of
@@ -31,12 +32,23 @@ parse input = case lines input of
         (key : values) = words line
 
 solve :: Maze -> Maybe Solution
-solve (Maze start end graph) = bfs [(start, [start])] M.empty
+solve (Maze start end graph) = bfs [(start, [start])] S.empty
   where
-    bfs :: [(Int, [Int])] -> M.Map Int [Int] -> Maybe Solution
+    bfs :: [(Int, [Int])] -> S.Set Int -> Maybe Solution
     bfs [] _ = Nothing -- Queue is empty, so no solution
     bfs ((currentVertex, currentPath) : restOfQueue) visited
       | currentVertex == end = Just (Solution currentPath) -- Found the end vertex, return the solution
-      | otherwise = case M.lookup currentVertex graph of
-          Just neighbors -> undefined
-          Nothing -> Nothing -- this node is a dead end
+      -- Haven't found the end yet, so keep searching
+      | otherwise = case M.lookup currentVertex graph of -- get current vertex's neighbors from map
+          Just neighbors ->
+            -- only look at neighbors that haven't been visited yet
+            let newNeighbors = filter (`S.notMember` visited) neighbors
+                -- for each neighbor make a tuple of (neighbor, path to neighbor)
+                -- where the path is the current path plus the neighbor
+                newPaths = [(n, currentPath ++ [n]) | n <- newNeighbors]
+                -- and these new paths to the queue
+                newQueue = restOfQueue ++ newPaths
+                -- and add these new neighbors to the visited set
+                newVisited = foldl (\set (vertex, _) -> S.insert vertex set) visited newPaths
+             in bfs newQueue newVisited -- continue searching with the new queue and visited set
+          Nothing -> bfs restOfQueue visited -- this node is a dead end, so just continue checking the rest of the queue
